a337a27dc84e4f7a9c113f3b31473ede648b2140
Nathan Reese <nreese@sect.net>
2019-07-19 16:57:34 -0400

    what is light without dark

--- s3-age/README.md
+++ s3-age/README.md
@@ -1,1 +1,23 @@
-I give you some interesting stats on S3 buckets.
+This little program connects to S3 using your aws credentials.
+
+Then she asks for a list of buckets and tells you which ones are over 1 years old. You can change that with a const.
+
+Then she dives in to each bucket and shows you which files have a LastModified over 3 years old. You can also adjust that.
+
+So it helps you find old stale things you are paying for.
+
+Sadly there is no way to tell when the file was ACCESSED with S3, only modified.
+
+The code now is mostly me thinking out loud, so to speak. I still should:
+
+1. Update the bucket eval to fill an object with files that meet
+age criteria. So it doesnt say the name of empty buckets OR buckets with
+nothing interesting in them.
+
+2. Return the object, probably stringified. Then you can consume it
+as you wish. 
+
+3. Accept a parameter, in case you have multiple profiles in your credentials and want to switch between them easily.
+
+4. That of course lays the groundwork to turn this all in to a microservice behind
+something like Express and let you query it from your own infrastructure management portal. You have one of those, right?!
--- s3-age/s3-age.js
+++ s3-age/s3-age.js
@@ -2,11 +2,12 @@ var AWS = require('aws-sdk');
 
 // evaluate buckets to determine age and report old ones
 let ageBuckets = (data) => {
+    console.log(`Evaluating age of your buckets...`)
     for (i in data.Buckets) {
         let myBucketDate = new Date(data.Buckets[i].CreationDate);
         let result = today.getFullYear() - myBucketDate.getFullYear();
         // console.log(data);
-        if (result >= maxBucketAge) console.log(`${data.Buckets[i].Name} is over ${maxBucketAge} year old.`); 
+        if (result >= maxBucketAge) console.log(`${data.Buckets[i].Name} \t is over ${maxBucketAge} year old.`); 
     }
 }
 
@@ -14,25 +15,30 @@ let ageBuckets = (data) => {
 let evaluateObjects = (objData) => {
     for (i in objData.Contents) {
          let currentObjDate = new Date(objData.Contents[i].LastModified)
+         // console.log(currentObjDate, today.getFullYear(), currentObjDate.getFullYear());
          let result = today.getFullYear() - currentObjDate.getFullYear();
-         if (result > maxObjectAge) console.log(`${objData.Contents[i].Key} is ${result} years old`);
+         if (result >= maxObjectAge) console.log(`${objData.Contents[i].Key} \t LastModified: ${result} years`);
     }
 }
 
 let ageBucketObjects = (data) => {
-    var params = {
-        Bucket: data.Buckets[0].Name,   //Index 0 for now.
-        // Delimiter: 'STRING_VALUE',  // using '/' would be interesting
-        MaxKeys: '1000',
-        // StartAfter: 'STRING_VALUE'  // subsequent calls cursor perhaps?
-    };
-    s3.listObjectsV2(params, function(objErr, objData) {
-        if (objErr) {
-            console.log(objErr, objErr.stack);
-        } else {
-            evaluateObjects(objData);
-        }
-    });
+    for (i in data.Buckets) {
+        var params = {
+            Bucket: data.Buckets[i].Name,
+            // Delimiter: 'STRING_VALUE',  // using '/' would be interesting
+            MaxKeys: '1000',
+            // StartAfter: 'STRING_VALUE'  // subsequent calls cursor perhaps?
+        };
+        s3.listObjectsV2(params, function(objErr, objData) {
+            if (objErr) {
+                console.log(objErr, objErr.stack);
+            } else {
+                console.log(`Evaluating age of objects in bucket ${objData.Name}`)
+                evaluateObjects(objData);
+                console.log(`\n \n \n`);
+            }
+        });
+    }
 }
 
 var processBuckets = function() {
@@ -41,6 +47,7 @@ var processBuckets = function() {
             console.log("Error:", err);
         } else {
             ageBuckets(data);
+            console.log(`\n \n \n`);
             ageBucketObjects(data);
         }
     })
@@ -50,6 +57,9 @@ var processBuckets = function() {
 const today = new Date();
 const maxBucketAge = 1;
 const maxObjectAge = 3;
+
+var credentials = new AWS.SharedIniFileCredentials({profile: 'rnd'});
+AWS.config.credentials = credentials;
 s3 = new AWS.S3({region: 'us-east-2', apiVersion: '2006-03-01'});
 
 processBuckets();
